f27461558e5107e3050a4e3caf70013dea2fd8ec
spnuac <spnuac@gmail.com>
2024-04-25 21:10:53 +0330

    INNERPRINTER sunmi t2s

--- escposprinter/src/main/java/com/dantsu/escposprinter/connection/bluetooth/BluetoothPrintersConnections.java
+++ escposprinter/src/main/java/com/dantsu/escposprinter/connection/bluetooth/BluetoothPrintersConnections.java
@@ -51,12 +51,19 @@ public class BluetoothPrintersConnections extends BluetoothConnections {
         for (BluetoothConnection bluetoothConnection : bluetoothDevicesList) {
             BluetoothDevice device = bluetoothConnection.getDevice();
 
-            int majDeviceCl = device.getBluetoothClass().getMajorDeviceClass(),
-                    deviceCl = device.getBluetoothClass().getDeviceClass();
 
-            if (majDeviceCl == BluetoothClass.Device.Major.IMAGING && (deviceCl == 1664 || deviceCl == BluetoothClass.Device.Major.IMAGING)) {
+            if(TextUtils.equals(device.getName().toUpperCase(),'INNERPRINTER')){
                 printersTmp[i++] = new BluetoothConnection(device);
             }
+            else{
+                int majDeviceCl = device.getBluetoothClass().getMajorDeviceClass(),
+                        deviceCl = device.getBluetoothClass().getDeviceClass();
+
+                if (majDeviceCl == BluetoothClass.Device.Major.IMAGING && (deviceCl == 1664 || deviceCl == BluetoothClass.Device.Major.IMAGING)) {
+                    printersTmp[i++] = new BluetoothConnection(device);
+                }
+            }
+
         }
         BluetoothConnection[] bluetoothPrinters = new BluetoothConnection[i];
         System.arraycopy(printersTmp, 0, bluetoothPrinters, 0, i);
