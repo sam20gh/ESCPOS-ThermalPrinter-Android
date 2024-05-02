8ba3ba38e07d20a83ddf74af8eedf9407dcb8db5
spnuac <spnuac@gmail.com>
2024-05-02 15:43:09 +0330

    v1.8

--- escposprinter/src/main/java/com/dantsu/escposprinter/connection/bluetooth/BluetoothConnection.java
+++ escposprinter/src/main/java/com/dantsu/escposprinter/connection/bluetooth/BluetoothConnection.java
@@ -94,6 +94,18 @@ public class BluetoothConnection extends DeviceConnection {
             this.disconnect();
             throw new EscPosConnectionException("Unable to connect to bluetooth device.");
         }
+        catch (NoSuchMethodException e) {
+            Log.d("LOGGRUBPRINTER","Unable to connect to bluetooth device NoSuchMethodException.");
+            e.printStackTrace();
+            this.disconnect();
+            throw new NoSuchMethodException(e.getMessage());
+        }
+        catch (IllegalAccessException e) {
+            Log.d("LOGGRUBPRINTER","Unable to connect to bluetooth device IllegalAccessException.");
+            e.printStackTrace();
+            this.disconnect();
+            throw new IllegalAccessException(e.getMessage());
+        }
         return this;
     }
 
